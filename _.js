0bdc280cc6df5fad3e9142b1e8065bba0dee4604
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-02-10 23:04:25 +0300

    added function inRange (checks if provided number is in the range)

--- _.js
+++ _.js
@@ -8,11 +8,29 @@ const _ = {
         return upperBound;
       else
         return number;
-    }
+    },
+    // checking if the number is within the range
+    inRange (number, startValue, endValue) {
+        // if start value is bigger then end value we swap them
+        if (startValue > endValue) {
+          let buff = 0;
+          buff = startValue;
+          startValue = endValue;
+          endValue = buff;
+        }
+        // if there is no end value we assign start value to 0
+        // and end value to start value
+        if (!endValue) {
+          endValue = startValue;
+          startValue = 0;
+        }
+        // if provided number is bigger or smaller then these numbers
+        // it means that the number is smaller or bigger than assigned range
+        if(number < startValue || number >= endValue)
+          return false;
+        // if the number is in the range the function returns true
+        return true;
+    },
   };
-
-
-
-
 
 module.exports = _;
