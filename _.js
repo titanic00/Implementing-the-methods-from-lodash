19ca439a603e82b648717136ab6c46e6be55fdb0
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-02-10 21:21:38 +0300

    added clamp function

--- _.js
+++ _.js
@@ -1,4 +1,15 @@
-
+const _ = {
+    clamp (number, lowerBound, upperBound) {
+      // checking if the provided number within two bounds or not
+      // if it't not the corresponding number will be returned
+      if (number < lowerBound)
+        return lowerBound;
+      else if (number > upperBound)
+        return upperBound;
+      else
+        return number;
+    }
+  };
 
 
 
